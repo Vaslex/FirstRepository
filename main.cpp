d6af27788b89140387a53cc47d3dcbe8e31bba65
Your Name <vaslex2007@gmail.com>
2023-04-12 20:21:21 +0500

    com

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject2.cpp: определяет точку входа для приложения.
+//
+
+#include <algorithm>
+#include "inout.h"//подключение inout.h,чтобы main cpp могла взаимодействовать с объявленными там функциями и библиотеками
+int main() {
+    vector<int> vec;//создание вектора 
+    read(vec);//вызов функции read
+    sort(vec.begin(), vec.end());//сортировка вектора
+    print(vec);//вызов функции print
+    return 0;
+}
